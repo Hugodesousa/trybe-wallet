76f1c65317a9e912ca0967d161d3df1bd81780dd
Hugodesousa <hugodesousa.16@gmail.com>
2022-07-28 19:33:38 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!! teste</div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" component={ Login } exact />
+        <Route path="/carteira" component={ Wallet } />
+      </Switch>
+
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,103 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { onLogin } from '../redux/actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emailInput: '',
+      passWordlInput: '',
+      buttonLogin: true,
+      login: false,
+    };
+  }
+
+  checkEmail = (emailInput) => {
+    const validEmail = new RegExp(
+      '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$',
+    );
+    return validEmail.test(emailInput);
+  }
+
+  checkLogin = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    }, () => {
+      const minPass = 6;
+      const { emailInput, passWordlInput } = this.state;
+
+      if (this.checkEmail(emailInput) && passWordlInput.length >= minPass) {
+        return this.setState({
+          buttonLogin: false,
+        });
+      }
+      return this.setState({
+        buttonLogin: true,
+      });
+    });
+  }
+
+  saveLogin = () => {
+    const { addEmail } = this.props;
+    const { emailInput } = this.state;
+    addEmail(emailInput);
+    this.setState({
+      login: true,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { buttonLogin, emailInput, passWordlInput, login } = this.state;
+    return (
+      <div>
+        <h1> Login </h1>
+        <form>
+          <label htmlFor="emailInput">
+            <input
+              id="emailInput"
+              name="emailInput"
+              placeholder="Email"
+              type="email"
+              data-testid="email-input"
+              onChange={ this.checkLogin }
+              value={ emailInput }
+            />
+          </label>
+          <label htmlFor="passWordlInput">
+            <input
+              placeholder="Senha"
+              id="passWordlInput"
+              name="passWordlInput"
+              type="password"
+              data-testid="password-input"
+              onChange={ this.checkLogin }
+              value={ passWordlInput }
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ buttonLogin }
+            onClick={ () => this.saveLogin() }
+          >
+            Entrar
+          </button>
+        </form>
+        {login ? <Redirect to="/carteira" /> : <> </>}
+      </div>);
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  addEmail: (payload) => {
+    dispatch(onLogin(payload));
+  },
+});
+
+Login.propTypes = {
+  addEmail: PropTypes.string.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
